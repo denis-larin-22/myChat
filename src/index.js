bd8af9ac86949bd774f9e6469821a9daf1f8164c
denis-larin-22 <denislarin2017@gmail.com>
2023-06-26 18:53:14 +0300

    Connect root component with react renderer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import { store } from './store';
+import { Root } from './views/Root';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
-      <App />
+      <Root/>
     </ReduxProvider>
   </React.StrictMode>
 );
--- src/views/Root/index.js
+++ src/views/Root/index.js
@@ -1,3 +1,3 @@
-export function Root() {
-
+export const Root = () => {
+    return null
 }
