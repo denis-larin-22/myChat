d3de9e83343a2a60828c613e7d7a20c219c57f7b
denis-larin-22 <denislarin2017@gmail.com>
2023-06-26 20:03:03 +0300

    Added theme at ThemeProvider, added SignUp and SignIn views

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { Provider as ReduxProvider } from 'react-redux';
 
 import { store } from './store';
 import { Root } from './views/Root';
+import { ThemeProvider } from '@emotion/react';
+import theme from './core/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
-      <Root/>
+      <ThemeProvider theme={theme}>
+        <Root/>
+      </ThemeProvider>
     </ReduxProvider>
   </React.StrictMode>
 );
