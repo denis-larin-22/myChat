b965b99b89bfce3801f62c7839ba5110923928d9
denis-larin-22 <denislarin2017@gmail.com>
2023-06-26 18:37:31 +0300

    Added redux, react-redux, basic redux code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider as ReduxProvider } from 'react-redux';
+
+import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>
 );
---
+++ src/store/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const initialState = {};
+
+const reducer = (state = initialState, action) => state ;
+
+export const rootReducer = combineReducers({reducer});
